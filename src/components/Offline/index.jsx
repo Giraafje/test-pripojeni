569cb2b82b2c07280f2edbdee282042c86657947
Giraafje <lenka.erbenova79@gmail.com>
2023-10-05 20:56:47 +0200

    Application is working as expected.

--- src/components/Offline/index.jsx
+++ src/components/Offline/index.jsx
@@ -3,6 +3,26 @@ import React, { useEffect, useState } from 'react';
 const Offline = () => {
   const [isOnline, setIsOnline] = useState(navigator.onLine);
 
+  useEffect(
+    () => {
+    console.log('mounted')
+    window.addEventListener('offline', handleOffline);
+    window.addEventListener('online', handleOnline);
+    return () => {
+      console.log('unmounted');
+      window.removeEventListener('offline', handleOffline);
+      window.removeEventListener('online', handleOnline);
+      }
+    }, [] //lepsi davat prazdne dependencies, aby se zabranilo zbytecnemu updatovani
+  )
+
+  const handleOffline = () => {
+    console.log('Calling handleOffline')
+    setIsOnline(false)
+  }
+
+  const handleOnline = () => setIsOnline(true)
+
   if (isOnline) {
     return (
       <p>Všechno v pořádku</p>
